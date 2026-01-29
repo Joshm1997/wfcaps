ad61555292276834d2a8fee321522c1e37bb586e
Joshm1997 <joshmathewshome@gmail.com>
2026-01-29 14:33:54 -0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ marginBottom: 8 }}>WFcaps</h1>
+      <p style={{ marginTop: 0, opacity: 0.8 }}>
+        Site is live. Next step: load your hats from a CSV export and link out to eBay.
+      </p>
+    </main>
+  );
+}
